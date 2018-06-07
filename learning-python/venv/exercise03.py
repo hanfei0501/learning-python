4c9369a00dfc7ce6081fb53cc38710563662f8ed
fhan <hanfei0501@outlook.com>
2018-06-07 23:03:12 +0800

    init repo.

---
+++ learning-python/venv/exercise03.py
@@ -0,0 +1,4 @@
+dollar = 100
+exchange_rate = 6.3907
+
+print("100美元可以兑换人民币：%f" % (dollar*exchange_rate))
---
+++ learning-python/venv/exercise04.py
@@ -0,0 +1,30 @@
+# str1 = 'Hello Python'
+# str2 = 'Let\'s go'
+# str3 = '"The Zen of Python" -- by Tim Peters'
+#
+# print(str1)
+# print(str2)
+# print(str3)
+
+# xyz = "xyz"
+# abc = "abc"
+#
+# print(xyz+abc)
+# print(xyz[1])
+# print(abc[2])
+# print(abc * 10)
+# print('a' in xyz)
+# print('a' in abc)
+
+# list = [1,2,3,4,5]
+# list.append(100)
+# list.remove(4)
+# print(list)
+# print(list[0:3])
+# print(list[-1])
+
+tup1 = (1,2,3,4,5)
+print(tup1[-2])
+tup2 = (6,7,8,9,10)
+tup = tup1+tup2
+print(len(tup))
